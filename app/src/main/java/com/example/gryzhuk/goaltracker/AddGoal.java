74b8e8c4a54bdc1a2d414ff2e0774fa2a0b7cb60
plumberg <hana.ryzhuk@gmail.com>
2018-12-24 16:59:54 -0500

    Changed FAB, added second activity, datepicker

---
+++ app/src/main/java/com/example/gryzhuk/goaltracker/AddGoal.java
@@ -0,0 +1,37 @@
+package com.example.gryzhuk.goaltracker;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+public class AddGoal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_goal);
+        setupFAB();
+       // setupDone();
+    }
+    public void setupFAB() {
+        Button done = findViewById(R.id.done_btn);
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setupDone();
+               // finish();
+            }
+        });
+    }
+
+    public void setupDone(){
+        DatePicker picker;
+        picker=(DatePicker)findViewById(R.id.datePicker1);
+        Toast.makeText(this,picker.getDayOfMonth()+"/"+ (picker.getMonth() + 1)+"/"+picker.getYear(),Toast.LENGTH_LONG).show();
+
+    }
+}
